5489a5849bbb2978a121d19e9241781c4da980ad
Unknown <ludovic.marchand@hotmail.be>
2018-04-18 19:18:57 +0200

    styling pt.1
    login / navigation begin

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
