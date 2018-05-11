b323f68d7db2a03adb7dc9b360a4919093278282
Unknown <ludovic.marchand@hotmail.be>
2018-05-11 16:18:40 +0200

    no currentDataItem -> warning message instead

--- src/app/data-item-request/data-item-request-detail/data-item-request-detail.component.ts
+++ src/app/data-item-request/data-item-request-detail/data-item-request-detail.component.ts
@@ -1,10 +1,7 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { DataItem } from '../../model/data-item';
-import { Room } from '../../model/room';
-import { isNullOrUndefined } from 'util';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { DataItemService } from '../../service/data-item.service';
-import { DataItemDetailComponent } from '../../data-item/data-item-detail/data-item-detail.component';
 import { DataItemRequest } from '../../model/data-item-request';
 import { DataItemRequestService } from '../../service/data-item-request.service';
 import { Vector3 } from '../../model/vector3';
@@ -18,7 +15,6 @@ export class DataItemRequestDetailComponent implements OnInit {
 
   dataItemRequest: DataItemRequest;
   @Input() dataItemName: string;
-  hasCurrentDataItem: Boolean = false;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private data: { dataItemRequest: DataItemRequest },
@@ -30,10 +26,6 @@ export class DataItemRequestDetailComponent implements OnInit {
   ngOnInit() {
     this.dataItemRequest = this.data.dataItemRequest;
     this.dataItemName = this.dataItemRequest.dataItemName;
-
-    if (this.dataItemRequest.currentDataItem !== null) {
-      this.hasCurrentDataItem = true;
-    }
   }
 
   onAcceptClick() {
@@ -42,10 +34,6 @@ export class DataItemRequestDetailComponent implements OnInit {
     dataItem.roomId = this.dataItemRequest.room.id;
     dataItem.location = new Vector3();
     dataItem.itemId = 'temp_item_id';
-
-    if (this.hasCurrentDataItem) {
-      this.dataItemService.delete(this.dataItemRequest.currentDataItem);
-    }
 
     this.dataItemService.create(dataItem);
     this.responseHandler();
