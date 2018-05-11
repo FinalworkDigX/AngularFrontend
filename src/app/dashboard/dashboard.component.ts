895f844c5c333e231e09108f9f6dd9891ecff279
Unknown <ludovic.marchand@hotmail.be>
2018-05-11 19:13:53 +0200

    design changes ++
    commit before stompjs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { Login } from '../model/login';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
